0db0db13142e0fd52f4f5d4d1ce86cddb3580def
liuxing <liuxingcoco@icloud.com>
2022-04-27 09:12:07 +0800

    初始化

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -2,6 +2,6 @@ import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
   final List _tabs = [
-    {'icon': CustomIcon.access_alarm}
+    // {'icon': CustomIcon.access_alarm}
   ];
 }
