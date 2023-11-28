fb4476b1def056315f55e3deb84daaf9a4cec610
xingliu <liuxingcoco@iCloud.com>
2023-11-28 14:05:55 +0800

    feat: 格式化

--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,17 @@ import 'package:flutter/material.dart';
 import 'pages/main/main_page.dart';
 
 void main() async => {
-      WidgetsFlutterBinding.ensureInitialized(),
-      await SystemChrome.setPreferredOrientations(
-        [
-          DeviceOrientation.portraitUp, // 竖屏 Portrait 模式
-          DeviceOrientation.portraitDown,
-          // DeviceOrientation.landscapeLeft, // 横屏 Landscape 模式
-          // DeviceOrientation.landscapeRight,
-        ],
-      ),
-      runApp(MyApp())
-    };
+  WidgetsFlutterBinding.ensureInitialized(),
+  await SystemChrome.setPreferredOrientations(
+    [
+      DeviceOrientation.portraitUp, // 竖屏 Portrait 模式
+      DeviceOrientation.portraitDown,
+      // DeviceOrientation.landscapeLeft, // 横屏 Landscape 模式
+      // DeviceOrientation.landscapeRight,
+    ],
+  ),
+  runApp(MyApp())
+};
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '网抑云音乐',
+      title: 'Stark X',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.pink),
       home: IndexPage(),
